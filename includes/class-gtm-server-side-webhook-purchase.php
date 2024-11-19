7df01935e39077b81351ff0dc6c1399d9108cc66
Mindaugas Budreika <mindaugas.bu@gmail.com>
2024-11-19 16:26:21 +0400

    add filters to processing, purchase and refund webhooks to allow payload data alterations

--- includes/class-gtm-server-side-webhook-purchase.php
+++ includes/class-gtm-server-side-webhook-purchase.php
@@ -73,6 +73,14 @@ class GTM_Server_Side_Webhook_Purchase {
 			}
 		}
 
+		/**
+		 * Allows modification of purchase webhook payload.
+		 *
+		 * @param array  $request Webhook payload data.
+		 * @param object $order   WC_Order instance.
+		 */
+		$request = apply_filters( 'gtm_server_side_purchase_webhook_payload', $request, $order );
+
 		GTM_Server_Side_Helpers::send_webhook_request( $request );
 	}
 }
